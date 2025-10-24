3451cfa6364153894e15bfddf591ac8326160b5f
Kamil <kkukielka@griddynamics.com>
2025-10-24 16:18:17 +0200

    Added UI layout for Data Generation / Prompt section

--- src/data_generation.py
+++ src/data_generation.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.text_input("Prmpt", placeholder="Enter your prompt here")
+prompt_container = st.container(border=True)
+
+with prompt_container:
+    st.text_input("Prompt", placeholder="Enter your prompt here")
+    upload_ddl_schema = st.file_uploader("Upload DDL Schema")
+    temperature = st.slider('Temperature')
+    max_tokens = st.text_input("Max tokens", value="100")
+    generate_button = st.button("Generate")
