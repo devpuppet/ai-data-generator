2cd9cf65a0024947755cb4dac2c3ed8efc488501
Kamil <kkukielka@griddynamics.com>
2025-10-28 14:56:03 +0100

    - Add functionality to send prompt with selected DDL file - update README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AI Data Generator
 
 In order to run `__main__.py` you need GEMINI_API_KEY environment variable
+
+Run the application:
+```
+GEMINI_API_KEY=your_key python -m streamlit run src/__main__.py
+```
+
--- src/data_generation.py
+++ src/data_generation.py
@@ -1,12 +1,27 @@
 import streamlit as st
 from src.service.gemini_service import GeminiAIService
+from io import StringIO
 
 prompt_container = st.container(border=True)
 gemini_ai_service = GeminiAIService()
 
 with prompt_container:
     prompt = st.text_input("Prompt", placeholder="Enter your prompt here")
-    upload_ddl_schema = st.file_uploader("Upload DDL Schema")
+    upload_ddl_schema_file = st.file_uploader("Upload DDL Schema")
     temperature = st.slider('Temperature')
     max_tokens = st.text_input("Max tokens", value="100")
-    generate_button = st.button("Generate", on_click=gemini_ai_service.generate_response, args=(prompt,))
+
+    def generate():
+        if not prompt:
+            st.warning("Please enter a prompt")
+            return
+        if upload_ddl_schema_file is None:
+            st.warning("Please select a DDL file")
+            return
+        schema = StringIO(upload_ddl_schema_file.getvalue().decode("utf-8")).read()
+        response = gemini_ai_service.generate_response(prompt + "\n`" + schema + "`")
+
+    generate_button = st.button("Generate", on_click=generate)
+
+
+
